82eeada84e6c7d89853fcd349015b6115efb3136
julian820568 <julian050682@gmail.com>
2014-06-26 01:58:07 +0800

    Update TocHW3.java

--- TocHW3.java
+++ TocHW3.java
@@ -10,7 +10,7 @@ import java.io.*;
 import java.net.*;
 import java.util.regex.*;
 
-public class TocHW3 {
+public class TocHw3 {
 	static URL url;
 	
 	public static void main(String[] args) throws IOException {
